ffe224e7db7038dbd35f3c40bdc29a944f417028
liorsolomon <liorsolomon@gmail.com>
2013-02-21 07:05:46 -0500

    Update src/main/resources/import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
-INSERT INTO T_RP_MONITORED_MARKETS VALUES(1, "Google", "https://play.google.com/store/apps/details?id=com.triplayinc.mmc&hl=en");
-INSERT INTO T_RP_MONITORED_MARKETS VALUES(2, "Apple", "https://itunes.apple.com/us/app/mymusiccloud/id460035384?mt=8");
---INSERT INTO T_RP_MONITORED_MARKETS VALUES(3, "BlackBerry", "https://appworld.blackberry.com/webstore/content/56385/");
+INSERT INTO T_RP_MONITORED_MARKETS VALUES(1, "Google", "https://play.google.com/store/apps/details?id=com.dropbox.android&hl=en");
+INSERT INTO T_RP_MONITORED_MARKETS VALUES(2, "Apple", "https://itunes.apple.com/us/app/dropbox/id327630330?mt=8");
